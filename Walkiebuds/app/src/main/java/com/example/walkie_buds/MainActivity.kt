531de892ee120f804776f0d9b713488490eb5a55
gwit13 <gwit0513@gmail.com>
2020-07-28 12:05:35 -0400

    Simplify code, implement crackly but sorta working feedback

--- Walkiebuds/app/src/main/java/com/example/walkie_buds/MainActivity.kt
+++ Walkiebuds/app/src/main/java/com/example/walkie_buds/MainActivity.kt
@@ -3,36 +3,38 @@ package com.example.walkie_buds
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.media.MediaPlayer
-import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
+import android.media.*
 import android.os.Bundle
 import android.util.Log
+import android.view.View.OnClickListener
 import android.view.ViewGroup
-
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import java.io.IOException
 
+
 private const val LOG_TAG = "WalkieBuds"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+private const val RECORDER_SAMPLERATE = 44100
+private const val RECORDER_CHANNELS = 16
+private const val RECORDER_AUDIO_ENCODING = 2
+
 
 class MainActivity : AppCompatActivity() {
     private var fileName: String = ""
 
     private var recordButton: RecordButton? = null
-    private var recorder: MediaRecorder? = null
+    private var recorder: AudioRecord? = null
 
-    private var playButton: PlayButton? = null
     private var player: MediaPlayer? = null
 
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
+    private lateinit var at: AudioTrack
+    private var isRecording = false
+
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -50,53 +52,66 @@ class MainActivity : AppCompatActivity() {
         stopRecording()
     }
 
-    private fun onPlay(start: Boolean) = if (start) {
-        startPlaying()
-    } else {
-        stopPlaying()
-    }
-
-    private fun startPlaying() {
-        player = MediaPlayer().apply {
-            try {
-                setDataSource(fileName)
-                prepare()
-                start()
-            } catch (e: IOException) {
-                Log.e(LOG_TAG, "prepare() failed")
-            }
-        }
-    }
-
-    private fun stopPlaying() {
-        player?.release()
-        player = null
-    }
 
     private fun startRecording() {
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile(fileName)
 
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        var bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+            RECORDER_AUDIO_ENCODING)
+//        println(bufferSize)
+//        bufferSize += 2048
+        var bytesPerElement = 2
 
+        recorder = AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, bufferSize*bytesPerElement)
+
+        recorder!!.startRecording()
+
+        isRecording = true
+
+        var recordingThread = Thread(Runnable {
             try {
-                prepare()
-            } catch(e: IOException) {
-                Log.e(LOG_TAG, "prepare() failed")
+                val intSize = AudioTrack.getMinBufferSize(
+                    RECORDER_SAMPLERATE,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    RECORDER_AUDIO_ENCODING
+                )
+                val sData = ByteArray(RECORDER_SAMPLERATE+128)
+                at = AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    RECORDER_SAMPLERATE,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    RECORDER_AUDIO_ENCODING,
+                    intSize,
+                    AudioTrack.MODE_STREAM
+                )
+                while (isRecording) {
+                    recorder!!.read(sData, 1, bufferSize) //isRecording = false; onStop button
+                    if (at != null) {
+                        at.play()
+                        // Write the byte array to the track
+                        println(at.write(sData, 0, sData.size))
+                        at.stop()
+                        //at.release()
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
+        }, "Transmitter Thread")
+        recordingThread.start()
 
-            start()
-        }
     }
 
     private fun stopRecording() {
+        isRecording = false
+        at.stop()
+        at.release()
+
         recorder?.apply {
             stop()
             release()
         }
         recorder = null
+
     }
 
     internal inner class RecordButton(ctx: Context) : androidx.appcompat.widget.AppCompatButton(ctx) {
@@ -104,34 +119,19 @@ class MainActivity : AppCompatActivity() {
         var clicker: OnClickListener = OnClickListener {
             onRecord(mStartPlaying)
             text = when(mStartPlaying) {
-                true -> "Stop recording"
-                false -> "Start recording"
+                true -> "Stop transmitting"
+                false -> "Start transmitting"
             }
             mStartPlaying = !mStartPlaying
         }
 
         init {
-            text = "Start recording"
+            text = "Start transmitting"
             setOnClickListener(clicker)
         }
     }
 
-    internal inner class PlayButton(ctx: Context) : androidx.appcompat.widget.AppCompatButton(ctx) {
-        var mStartPlaying = true
-        var clicker: OnClickListener = OnClickListener {
-            onPlay(mStartPlaying)
-            text = when (mStartPlaying) {
-                true -> "Stop playing"
-                false -> "Start playing"
-            }
-            mStartPlaying = !mStartPlaying
-        }
 
-        init {
-            text = "Start playing"
-            setOnClickListener(clicker)
-        }
-    }
 
     override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
@@ -141,13 +141,8 @@ class MainActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
 
         recordButton = RecordButton(this)
-        playButton = PlayButton(this)
         val ll = LinearLayout(this).apply {
             addView(recordButton, LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    0f))
-            addView(playButton, LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     0f))
